0717c2af8f837e1349da00df3023499bbedd5ed8
Abel Begashaw <abelbegashawmekuria@gmail.com>
2026-01-03 23:51:56 +0300

    Working on the problem still

--- A_Sleeping_Through_Classes.py
+++ A_Sleeping_Through_Classes.py
@@ -1,3 +1,16 @@
 t = int(input())
 for _ in range(t):
-    print(_)
+    n, k = map(int, input().split())
+    s = input()
+    result = index = 0
+    while index < n:
+        if s[index] == '0':
+            result += 1
+            index += 1
+        else:
+            curr_one = index
+            for i in range(index, min(index + k + 1, n)):
+                if s[i] == '1':
+                    curr_one = i
+            index = curr_one + k + 1
+    print(result)
