ade16e66c5e730bd2fc1b5e767ac1b287b94b980
Abel Begashaw <abelbegashawmekuria@gmail.com>
2026-01-09 23:03:47 +0300

    Added a Solution for Codeforces Round 1056 Div 2 Problem B

--- A_Carnival_Wheel.py
+++ A_Carnival_Wheel.py
@@ -3,3 +3,13 @@ for _ in range(t):
     l, a, b = map(int, input().split())
     k = (l - a) // b
     print(a + k * b) if (l - a) % b else print(l - b)
+    """
+    0 1 2 3 4
+
+    (3 + 2K) % 5 = 4
+
+    2K % 5 = 1
+    
+    2K = 1
+    K = 1/2
+    """
