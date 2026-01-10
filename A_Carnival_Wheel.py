511b1cb9ac8516b76601e7644de41b9d0543c4fc
Abel Begashaw <abelbegashawmekuria@gmail.com>
2026-01-10 22:03:20 +0300

    Added Solution for Codeforces Global Round 31 (Div 1 + Div 2) Problem A

--- A_Carnival_Wheel.py
+++ A_Carnival_Wheel.py
@@ -1,15 +1,10 @@
 t = int(input())
 for _ in range(t):
     l, a, b = map(int, input().split())
-    k = (l - a) // b
-    print(a + k * b) if (l - a) % b else print(l - b)
-    """
-    0 1 2 3 4
-
-    (3 + 2K) % 5 = 4
-
-    2K % 5 = 1
-    
-    2K = 1
-    K = 1/2
-    """
+    wheel = [False for _ in range(l)]
+    result = a
+    while not wheel[a]:
+        result = max(result, a)
+        wheel[a] = True
+        a = (a + b) % l
+    print(result)
