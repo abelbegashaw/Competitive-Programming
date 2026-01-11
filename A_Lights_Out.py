72b6087e444aa82700f8c99b39836411a606f39f
Abel Begashaw <abelbegashawmekuria@gmail.com>
2026-01-11 21:20:04 +0300

    Mmodified the Solution for Codeforces Round 168 Div 2 Problem A

--- A_Lights_Out.py
+++ A_Lights_Out.py
@@ -1,20 +1,21 @@
 actions = [input().split() for _ in range(3)]
 result = [['1' for _ in range(3)] for _ in range(3)]
+
+def inbound(x, y):
+    return 0 <= x < 3 and 0 <= y < 3
+
+def toggle(x, y):
+    directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    result[x][y] = '1' if result[x][y] == '0' else '0'
+    for dx, dy in directions:
+        new_x, new_y = x + dx, y + dy
+        if inbound(new_x, new_y):
+            result[new_x][new_y] = '1' if result[new_x][new_y] == '0' else '0'
+
 for i in range(3):
     for j in range(3):
         if int(actions[i][j]) % 2:
-            result[i][j] = '1' if result[i][j] == '0' else '0'
-            if i != 0:
-                result[i - 1][j] = '1' if result[i - 1][j] == '0' else '0'
-            
-            if i != 2:
-                result[i + 1][j] = '1' if result[i + 1][j] == '0' else '0'
-            
-            if j != 0:
-                result[i][j - 1] = '1' if result[i][j - 1] == '0' else '0'
-            
-            if j != 2:
-                result[i][j + 1] = '1' if result[i][j + 1] == '0' else '0'
-            
+            toggle(i, j)
+
 for row in result:
     print("".join(row))
