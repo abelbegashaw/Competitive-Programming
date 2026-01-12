8529f81111959efc0e6208d505e0e48f6f60033e
Abel Begashaw <abelbegashawmekuria@gmail.com>
2026-01-12 16:15:16 +0300

    Added a Solution for Codeforces Round 143 Div 2 Problem A

--- A_HQ_9.py
+++ A_HQ_9.py
@@ -1,2 +1,2 @@
 program = input()
-print("YES") if "Q" in program or "H" in program or "9" in program or "+" in program else print("NO")
+print("YES") if "Q" in program or "H" in program or "9" in program else print("NO")
