2bd42054cc8ddede55d2afbaf35d6df7c6b5383b
Abel Begashaw <abelbegashawmekuria@gmail.com>
2026-01-13 23:32:37 +0300

    Modified the Solution for Codeforces Round 125 Div 2 Problem A

--- A_Hexadecimal_s_theorem.py
+++ A_Hexadecimal_s_theorem.py
@@ -1,20 +1,2 @@
 n = int(input())
-a, b = 0, 1
-fibs = [a, b]
-while b < 10**9:
-    a, b = b, a + b
-    fibs.append(b)
-flag = False
-for i in range((len(fibs))):
-    for j in range(len(fibs)):
-        for k in range(len(fibs)):
-            if fibs[i] + fibs[j] + fibs[k] == n:
-                print(fibs[i], fibs[j], fibs[k])
-                flag = True
-                break
-        if flag:
-            break
-    if flag:
-        break
-else:
-    print("I'm too stupid to solve this problem")
+print(0, 0, n)
