e8abca338f4b24f20065a465b5df7ffcdf095eb6
Abel Begashaw <abelbegashawmekuria@gmail.com>
2026-01-15 18:24:36 +0300

    Modified the Solution for Codeforces Round 162 Div 2 Problem A

--- A_Business_trip.py
+++ A_Business_trip.py
@@ -3,8 +3,8 @@ growth = list(map(int, input().split()))
 growth.sort()
 result = height = 0
 index = len(growth) - 1
-while k > 0:
+while index > -1 and k > 0:
     k -= growth[index]
     index -= 1
     result += 1
-print(result)
+print(result) if k <= 0 else print(-1)
