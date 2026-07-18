80c321d2515bdbe6a3ed0a71667b6bf3d766fa01
Abel Begashaw <abelbegashawmekuria@gmail.com>
2026-07-18 19:14:31 +0300

    Added the working solution for B_Asmhal problem

--- B_Ashmal.py
+++ B_Ashmal.py
@@ -4,12 +4,10 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     a = input().split()
-    queue = deque([])
+    result = ""
     for string in a:
-        if not queue:
-            queue.append(string)
-        elif string + queue[0] <= queue[0] + string:
-            queue.appendleft(string)
+        if string + result <= result + string:
+            result = string + result
         else:
-            queue.append(string)
-    print("".join(queue))
+            result = result + string
+    print(result)
